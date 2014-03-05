e316f8d3c5ccd29b6ef4c2387862d77aa4cf30cf
mautechr <christoph.maute@zuehlke.com>
2014-03-05 18:11:48 +0100

    Initialize project setup c# api

---
+++ scenarioo-cs/Scenarioo/ScenariooTest/ScenariooTest/ScenarioDocuWriterAndReaderTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScenariooTest
+{
+    [TestClass]
+    public class ScenarioDocuWriterAndReaderTest
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ scenarioo-cs/TestConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scenarioo.Api;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
