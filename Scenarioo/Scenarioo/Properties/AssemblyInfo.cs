c444ef2b59ead02cc3e9436bf7cabb7cfc5ccc35
mautechr <christoph.maute@zuehlke.com>
2014-07-28 19:24:23 +0200

    #217 moved .nuspec into .csproj folder

--- Scenarioo/Scenarioo/Properties/AssemblyInfo.cs
+++ Scenarioo/Scenarioo/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Scenarioo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("API for Scenarioo documentation content generation")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Scenarioo")]
 [assembly: AssemblyProduct("Scenarioo-CS")]
