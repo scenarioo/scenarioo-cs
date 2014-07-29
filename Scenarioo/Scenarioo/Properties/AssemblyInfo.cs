bc663c0452f8e71d3695130a3b1d8a97546ec801
mautechr <christoph.maute@zuehlke.com>
2014-07-29 14:13:36 +0200

    #217 changed assembly version no.; AppVeyor doesn't use the build no. for publishing NuGet package and provokes an conflict

--- Scenarioo/Scenarioo/Properties/AssemblyInfo.cs
+++ Scenarioo/Scenarioo/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Scenarioo")]
+[assembly: AssemblyTitle("Scenarioo-cs API")]
 [assembly: AssemblyDescription("API for Scenarioo documentation content generation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Scenarioo")]
-[assembly: AssemblyProduct("Scenarioo-CS")]
-[assembly: AssemblyCopyright("Scenarioo")]
+[assembly: AssemblyCompany("Scenarioo.org")]
+[assembly: AssemblyProduct("Scenarioo")]
+[assembly: AssemblyCopyright("Scenarioo.org")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.0.2")]
+[assembly: AssemblyFileVersion("0.1.0.2")]
